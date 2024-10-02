160a713684d714004713ec32082013bef69a9311
akuAldy <reinaldyrestuaji@gmail.com>
2024-10-02 14:58:25 +0700

    profile

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:tubes_pbp_6/profile.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(MaterialApp(
+    home: MyWidget(),
+  ));
 }
 
 class MainApp extends StatelessWidget {
