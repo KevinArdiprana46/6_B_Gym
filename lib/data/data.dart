1348c8c550192f472f4e69e46639ab499eb7f332
KadekDeva13 <220711929@students.uajy.ac.id>
2024-12-01 20:47:26 +0700

    Revert "Merge remote-tracking branch 'origin/Reinaldy-branch' into merge-branch-fix"
    This reverts commit d4b976d9e4abb66054c95cdf97458a2be0eaab6c, reversing
    changes made to 70f348ea7401b904a48db6f3eeb5f47dd332aa45.

--- lib/data/data.dart
+++ lib/data/data.dart
@@ -1,55 +1,28 @@
-class dataLayanan {
+class dataLayanan{
   final String namaLayanan;
   final String deskripsi;
   final String jadwal;
-  final String trainerImage;
-  final String classImage;
-
-  dataLayanan(
-    this.namaLayanan,
-    this.deskripsi,
-    this.jadwal,
-    this.trainerImage,
-    this.classImage,
-  );
+  dataLayanan(this.namaLayanan, this.deskripsi, this.jadwal);
 }
-final List<dataLayanan> data = _data.map(
-  (e) => dataLayanan(
-    e['namaLayanan'] as String,
-    e['deskripsi'] as String,
-    e['jadwal'] as String,
-    e['trainerImage'] as String,
-    e['classImage'] as String,
-  ),
-).toList(growable: false);
 
-final List<Map<String, Object>> _data = [
+final List<dataLayanan> data =
+  _data.map((e) => dataLayanan(e['namaLayanan'] as String, e['deskripsi'] as String, e['jadwal'] as String)).toList(growable: false);
+
+final List<Map<String, Object>> _data =
+[
   {
     "namaLayanan": "Kelas Kardio",
-    "deskripsi": "Kelas Kardio untuk kesehatan jantung dan membakar lemak",
-    "jadwal": "Setiap Selasa",
-    "trainerImage": "lib/assets/trainer.png",
-    "classImage": "lib/assets/gym.jpg",
+    "deskripsi" : "Kelas Kardio untuk kesehatan jantung dan membakar lemak",
+    "jadwal": "Setiap Selasa"
   },
   {
     "namaLayanan": "Kelas Angkat Beban",
-    "deskripsi": "Membangun Massa Otot",
-    "jadwal": "Setiap Kamis",
-    "trainerImage": "lib/assets/class.png",
-    "classImage": "lib/assets/gym.jpg",
+    "deskripsi" : "Membangun Massa Otot",
+    "jadwal": "Setiap Kamis"
   },
   {
     "namaLayanan": "Kelas Yoga",
-    "deskripsi": "Membangun ketenangan jiwa",
-    "jadwal": "Setiap Minggu",
-    "trainerImage": "lib/assets/class.png",
-    "classImage": "lib/assets/class.png",
-  },
-  {
-    "namaLayanan": "Boxing",
-    "deskripsi": "Kelas Boxing untuk latihan intens",
-    "jadwal": "Setiap Sabtu",
-    "trainerImage": "lib/assets/class.png",
-    "classImage": "lib/assets/trainer.png",
-  },
+    "deskripsi" : "Membangun ketenangan jiwa",
+    "jadwal": "Setiap Minggu"
+  }
 ];
