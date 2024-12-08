4aacc0587da4940e79f9a20f638d8370d036fd6c
AaronPanji <220711818@students.uajy.ac.id>
2024-12-09 00:41:12 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:tubes_pbp_6/helper/shared_preference.helper.entity.dart';
+import 'package:tubes_pbp_6/view/home.dart';
+import 'package:tubes_pbp_6/view/login_register/prelogin.dart';
 
-void main() {
-  runApp(const MainApp());
+void main() async {
+   WidgetsFlutterBinding.ensureInitialized();
+   await SharedPreferenceHelper.init();
+  runApp(MaterialApp(
+    home: PreLoginView(),
+  ));
 }
 
 class MainApp extends StatelessWidget {
@@ -10,11 +17,21 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hai'),
-        ),
-      ),
+      home: BerandaView(),
     );
+  }
+}
+
+class MyWidget extends StatefulWidget {
+  const MyWidget({super.key});
+
+  @override
+  State<MyWidget> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<MyWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
   }
 }
